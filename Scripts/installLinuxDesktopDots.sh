fbe3525dfaab3fa35d97135a80e0a679973c89eb
SolidHal <hal@halemmerich.com>
2020-04-12 23:56:30 -0700

    cleanup dots, rework desktop install script

---
+++ Scripts/installLinuxDesktopDots.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Usage: TryLinkDir source_path destination_path
+safeLinkDir(){
+    if [ ! -d $2 ]; then
+        ln -s $1 $2
+    else
+        echo "safeLinkDir: $2 already exists, not created"
+    fi
+}
+
+DOTS=~/dotfiles/dotfiles
+SHARED=$DOTS/shared
+DEVICE=$DOTS/desktop
+
+BASHDOTS=$SHARED/bash
+REGOLITHDOTS=$DEVICE/regolith
+#for laptop
+# DEVICE=$DOTS/laptop
+
+
+#shared dots
+ln -s $BASHDOTS/.inputrc ~/.inputrc || true
+ln -s $BASHDOTS/.bash_alises ~/.bash_aliases || true
+ln -s $SHARED/git/.gitconfig ~/.gitconfig || true
+ln -s $SHARED/spacemacs/.spacemacs ~/.spacemacs || true
+
+#device specific dots
+#regolith
+safeLinkDir $REGOLITHDOTS ~/.config/regolith
