d0d3da573dc5e351a30f779e90e1961b6334ada6
SolidHal <hal@halemmerich.com>
2022-07-25 17:23:50 -0500

    add syncthing, keepass to installed packages

--- scripts/shared_install_lib.sh
+++ scripts/shared_install_lib.sh
@@ -41,7 +41,15 @@ installPackages(){
     sudo apt install \
          zsh \
          emacs \
-         ripgrep
+         ripgrep \
+         keepassx
+
+    ## syncthing
+    sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+    echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+    sudo apt-get update
+    sudo apt-get install syncthing
+    systemctl --user enable syncthing.service
 }
 
 linkSharedDots(){
