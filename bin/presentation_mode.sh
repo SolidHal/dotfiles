502a7f5b597c2a9c6688a15f9970f827784a425c
SolidHal <hal@halemmerich.com>
2022-11-09 14:44:19 -0600

    add script to toggle presentation mode

---
+++ bin/presentation_mode.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if [ -z ${1+x} ]; then echo "pass one of: enable, disable"; exit 1; else echo "${1} presentation mode"; fi
+
+
+# alacritty font size, opacity
+if [ "$1" = "disable" ]; then
+    sed -i 's/size\: 15\.0/size\: 10\.0/g' ~/.alacritty.yml
+    sed -i 's/opacity\: 1\.0/opacity\: 0\.95/g' ~/.alacritty.yml
+
+elif [ "$1" = "enable" ]; then
+    sed -i 's/size\: 10\.0/size\: 15\.0/g' ~/.alacritty.yml
+    sed -i 's/opacity\: 0\.95/opacity\: 1\.0/g' ~/.alacritty.yml
+
+else
+    echo "pass one of: enable, disable"
+fi
+
+
