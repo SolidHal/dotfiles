032397af5fa26f070597cf388f7a711289fcae67
SolidHal <hal@halemmerich.com>
2022-12-13 20:15:46 -0600

    adjust presentation mode font size

--- bin/presentation_mode.sh
+++ bin/presentation_mode.sh
@@ -6,11 +6,11 @@ if [ -z ${1+x} ]; then echo "pass one of: enable, disable"; exit 1; else echo "$
 
 # alacritty font size, opacity
 if [ "$1" = "disable" ]; then
-    sed -i 's/size\: 15\.0/size\: 10\.0/g' ~/.alacritty.yml
+    sed -i 's/size\: 18\.0/size\: 10\.0/g' ~/.alacritty.yml
     sed -i 's/opacity\: 1\.0/opacity\: 0\.95/g' ~/.alacritty.yml
 
 elif [ "$1" = "enable" ]; then
-    sed -i 's/size\: 10\.0/size\: 15\.0/g' ~/.alacritty.yml
+    sed -i 's/size\: 10\.0/size\: 18\.0/g' ~/.alacritty.yml
     sed -i 's/opacity\: 0\.95/opacity\: 1\.0/g' ~/.alacritty.yml
 
 else
