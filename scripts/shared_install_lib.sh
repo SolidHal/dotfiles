88291bc757ab4c0292ffa73060671c5f1fed8f75
SolidHal <hal@halemmerich.com>
2022-12-13 20:18:27 -0600

    use dracula as default regolith look

--- scripts/shared_install_lib.sh
+++ scripts/shared_install_lib.sh
@@ -48,6 +48,10 @@ installPackages(){
          ripgrep \
          keepassx
 
+    ## regolith looks
+    sudo apt install \
+         regolith-look-dracula
+
     ## syncthing
     sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
     echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
